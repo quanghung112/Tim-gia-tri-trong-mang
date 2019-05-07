6a1c0dfa67d693a495d4fdcc9f2eb1dee3db0b21
quanghung112 <laiquanghung112@gmail.com>
2019-05-07 14:45:59 +0700

    Sua file Search.js

--- Search.js
+++ Search.js
@@ -1,16 +1,24 @@
 function Timkiem() {
-    let arr = [1, 4, 7, 23, 67, 65, 324, 532, 676, 32, 5, 7];
+    let arr = [1, 4, 7, 23, 67, 65, 4, 532, 676, 32, 5, 7];
     let e="<hr/>";
-    let x = [];
     for (i = 0; i < arr.length; i++) {
-         e += arr[i] + ", "
+        if (i<arr.length-1){
+            e += arr[i] + ", ";
+        }else{
+            e += arr[i];
+        }
     }
-    document.getElementById('mang').innerHTML = e;
+    document.getElementById('mang').innerHTML = e ;
     let a = document.getElementById('Input').value;
+    let dem =0;
     for (j = 0; j < arr.length; j++) {
         if (a == arr[j]) {
-            document.getElementById('show').innerHTML = " Co gia tri " + a + " tai vi tri " + j + " trong mang";
+            document.getElementById('show').innerHTML += " Co phan tu " + a + " tai vi tri " + j + " trong mang" + "<br>";
+            dem++;
         }
+    }
+    if (dem ==0 ){
+        document.getElementById('show').innerHTML="Khong co phan tu " + a + " trong mang"
     }
 }
 
